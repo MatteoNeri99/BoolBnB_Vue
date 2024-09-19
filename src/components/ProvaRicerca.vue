8215e6dd4f200e0df658235ed097e9a1fb0a46ec
MatteoNeri99 <matteolavoro99@gmail.com>
2024-09-19 08:53:41 +0200

    aggiunti services nei filtri di ricerca

--- src/components/ProvaRicerca.vue
+++ src/components/ProvaRicerca.vue
@@ -1,58 +1,3 @@
-<template>
-  <div class="apartment-search">
-    <h3>Search Apartments</h3>
-  <form >  
-    <div class="form-group">
-      <label for="indirizzo">Enter Address</label>
-      <input 
-        type="search" 
-        v-model="address" 
-        @input="fetchAutocomplete" 
-        class="form-control me-2 searchbar border-radius" 
-        id="indirizzo" name="indirizzo"  
-        placeholder="Inserisci un indirizzo" 
-        aria-label="Search">
-      <ul v-if="autocompleteResults.length" class="autocomplete-results">
-        <li v-for="result in autocompleteResults" :key="result.id" @click="selectAddress(result)">
-          {{ result.address.freeformAddress }}
-        </li>
-      </ul>
-      <p v-if="!isValidAddress && address.length > 0" class="text-danger">Please select a valid address from the list.</p>
-    </div>
-
-    <div class="row mt-3 justify-content-center">
-      <div class="col-md-2">
-          <label for="numStanze" class="form-label">Numero di stanze</label>
-          <input v-model="filters.Stanze" type="number" class="form-control" name="Stanze" id="Stanze" placeholder="Inserisci il numero di stanze">
-      </div>
-      <div class="col-md-2">
-          <label for="numLetti" class="form-label">Numero di posti letto</label>
-          <input v-model="filters.Letti" type="number" class="form-control" name="Letti" id="Letti" placeholder="Inserisci il numero di letti">
-      </div>
-      <div class="col-md-2">
-          <label for="numBagni" class="form-label">Numero di bagni</label>
-          <input v-model="filters.Bagni" type="number" class="form-control" name="Bagni" id="Bagni" placeholder="Inserisci il numero di bagni ">
-      </div>
-      <!-- Campo per il raggio di ricerca -->
-      <div class="col-md-2">
-        <label for="radius" class="form-label">Raggio di ricerca (km)</label>
-        <input v-model="radius" type="number" class="form-control" name="radius" id="radius" min="1" max="20" value="20" placeholder="20 km">
-        <small class="form-text text-muted">Il raggio di ricerca può essere modificato solo per ridurre il valore.</small>
-      </div>
-    </div>
-    <button class="btn btn-primary" @click.prevent="searchApartments" :disabled="!isValidAddress">Search</button>
-  </form>
-
-    <div v-if="apartments.length">
-      <h4>Search Results</h4>
-      <ul>
-        <li v-for="apartment in apartments" :key="apartment.id">
-          {{ apartment.Indirizzo }} - {{ apartment.Stanze }} Rooms
-        </li>
-      </ul>
-    </div>
-  </div>
-</template>
 
 <script>
 import axios from 'axios';
@@ -63,6 +8,7 @@ export default {
       address: '',
       radius: 20,
       autocompleteResults: [],
+      services:[],
       selectedCoordinates: null,
       isValidAddress: false,
       filters: {
@@ -108,6 +54,7 @@ export default {
 
     // Metodo per cercare appartamenti usando le API
     async searchApartments() {
+      console.log(this.address)
       if (!this.selectedCoordinates) {
         alert('Please select a valid address from the list.');
         return;
@@ -115,13 +62,16 @@ export default {
 
       try {
         const response = await axios.get('http://127.0.0.1:8000/api/apartments/search', {
-          indirizzo: this.address,
-          radius: this.radius,
-          Stanze: this.filters.Stanze,
-          Letti: this.filters.Letti,
-          Bagni: this.filters.Bagni,
-          lat: this.selectedCoordinates.lat,
-          lon: this.selectedCoordinates.lon,
+          params: {
+            indirizzo: this.address,
+            radius: this.radius,
+            Stanze: this.filters.Stanze,
+            Letti: this.filters.Letti,
+            Bagni: this.filters.Bagni,
+            services:this.services,
+            lat: this.selectedCoordinates.lat,
+            lon: this.selectedCoordinates.lon,
+          }
         });
 
         this.apartments = response.data;
@@ -132,6 +82,95 @@ export default {
   },
 };
 </script>
+<template>
+    <div class="apartment-search">
+      <h3>Search Apartments</h3>
+    <form >  
+      <div class="form-group">
+        <label for="indirizzo">Enter Address</label>
+        <input 
+          type="text" 
+          v-model="address" 
+          @input="fetchAutocomplete" 
+          class="form-control me-2 searchbar border-radius" 
+          id="Indirizzo" name="Indirizzo"  
+          placeholder="Inserisci un indirizzo" 
+          aria-label="Search">
+        <ul v-if="autocompleteResults.length" class="autocomplete-results">
+          <li v-for="result in autocompleteResults" :key="result.id" @click="selectAddress(result)">
+            {{ result.address.freeformAddress }}
+          </li>
+        </ul>
+        <p v-if="!isValidAddress && address.length > 0" class="text-danger">Please select a valid address from the list.</p>
+      </div>
+
+      <div class="row mt-3 justify-content-center">
+        <div class="col-md-2">
+            <label for="numStanze" class="form-label">Numero di stanze</label>
+            <input v-model="filters.Stanze" type="number" class="form-control" name="Stanze" id="Stanze" placeholder="Inserisci il numero di stanze">
+        </div>
+        <div class="col-md-2">
+            <label for="numLetti" class="form-label">Numero di posti letto</label>
+            <input v-model="filters.Letti" type="number" class="form-control" name="Letti" id="Letti" placeholder="Inserisci il numero di letti">
+        </div>
+        <div class="col-md-2">
+            <label for="numBagni" class="form-label">Numero di bagni</label>
+            <input v-model="filters.Bagni" type="number" class="form-control" name="Bagni" id="Bagni" placeholder="Inserisci il numero di bagni ">
+        </div>
+        <!-- Campo per il raggio di ricerca -->
+        <div class="col-md-2">
+          <label for="radius" class="form-label">Raggio di ricerca (km)</label>
+          <input v-model="radius" type="number" class="form-control" name="radius" id="radius" min="1" max="20" value="20" placeholder="20 km">
+          <small class="form-text text-muted">Il raggio di ricerca può essere modificato solo per ridurre il valore.</small>
+        </div>
+      </div>
+      <button class="btn btn-primary" @click.prevent="searchApartments" :disabled="!isValidAddress">Search</button>
+      <div class="container">
+        <div class="row">
+            <div class=" col-md-2 d-flex flex-column ">
+              <ul class="list-unstyled d-flex align-items-center  m-3" >
+                <li    class="p-3"><label><input type="checkbox" v-model="services" name="services[]"  value="1"> WiFi gratuito</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="2"> Colazione inclusa</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="3"> Aria condizionata</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="4"> Parcheggio gratuito</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="5"> Servizio in camera</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="6"> Animali ammessi</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="7"> Piscina</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="8"> Palestra</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="9"> Spa e centro benessere</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="10"> TV satellitare</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="11"> Minibar</label></li>
+                <li    class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="12"> Cassaforte in camera</label></li>
+              </ul>
+              <ul class="list-unstyled d-flex align-items-center  m-3" >
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="13"> Accesso per disabili</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="14"> Deposito bagagli</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="15"> Servizio navetta</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="16"> Noleggio biciclette</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="17"> Area giochi per bambini</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="18"> Sala conferenze</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="19"> Bar</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="20"> Ristorante</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="21"> Servizio lavanderia</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="22"> Asciugacapelli</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="23"> Ferro da stiro</label></li>
+                <li class="p-3"><label ><input type="checkbox" v-model="services" name="services[]" value="24"> Reception 24 ore su 24</label></li>
+              </ul>
+            </div>
+        </div>
+                    </div>
+    </form>
+
+      <div v-if="apartments.length">
+        <h4>Search Results</h4>
+        <ul>
+          <li v-for="apartment in apartments" :key="apartment.id">
+            {{ apartment.Indirizzo }} - {{ apartment.Stanze }} Rooms
+          </li>
+        </ul>
+      </div>
+    </div>
+</template>
 
 <style scoped>
 .autocomplete-results {
