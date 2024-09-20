7bf3e24c6c8c969c350141e9b552498921dc9f6b
MatteoNeri99 <matteolavoro99@gmail.com>
2024-09-20 09:35:37 +0200

    aggiunti servizi nelle card

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -2,12 +2,12 @@
     <div class="container">
       <div>
         <div class="row justify-content-center">
-          <div class="col-md-4 mb-4 w-100" v-for="apartment in apartments" :key="apartment.id">
+          <div class="col-md-4 mb-4 w-100" v-for="apartment in apartments"  :key="apartment.id">
             <div class="card p-3 w-100 styled-header">
               <img :src="apartment.Img" class="card-img-top" alt="Apartment Image">
               <div class="card-body">
                 <h5 class="card-title">{{ apartment.Nome }}</h5>
-                <ul class="list-unstyled">
+                <ul class="list-unstyled" >
                   <li class="fw-bold"><i class="fa-solid fa-euro-sign"></i> Prezzo: <span class="fw-normal">&euro;{{ apartment.Prezzo }}</span></li>
                   <li class="fw-bold"><i class="fa-solid fa-person-booth"></i> Stanze: <span class="fw-normal">{{ apartment.Stanze }}</span></li>
                   <li class="fw-bold"><i class="fa-solid fa-bed"></i> Letti: <span class="fw-normal">{{ apartment.Letti }}</span></li>
@@ -15,6 +15,7 @@
                   <li class="fw-bold"><i class="fa-solid fa-ruler"></i> Metri quadrati: <span class="fw-normal">{{ apartment.Metri_quadrati }} metri quadrati</span></li>
                   <li class="fw-bold"><i class="fa-solid fa-location-dot"></i> Indirizzo: <span class="fw-normal">{{ apartment.Indirizzo }}</span></li>
                 </ul>
+                <p class="fw-bold" Servizi: v-for="service in apartment.services">{{ service.Nome }}</p>
             </div>
             <div class="card-footer">
               <router-link :to="{ name: 'apartment-details', params: { id: apartment.id } }" class="btn btn-primary btn-submit">
